3ae96c66c172af99269150a08b93c25aaa38caec
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-06-30 21:40:35 +0200

    Convert main codebase to a Class pattern

--- git-jsontree.js
+++ git-jsontree.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const repository = require('gitty')(process.cwd());
+const gitty      = require('gitty');
 const Command    = require('gitty/lib/command.js');
 const parser     = require('gitty/lib/parser.js');
-const schema     = require('./default-schema.js');
+const schemaTpl  = require('./default-schema.js');
 
 const logFmt = JSON.stringify({
   id: '%H',
@@ -22,51 +22,71 @@ const branchTpl = {
   type: 'branch',
 };
 
-const getCommits = new Command(repository, 'log', ['-a', `--pretty=format:'${logFmt}',`, '']);
-const getBranches = new Command(repository, 'branch', ['--format="%(refname:short) %(objectname:)"']);
+const getCommitsCmd = repository =>
+  new Command(repository, 'log', ['-a', `--pretty=format:'${logFmt}',`, '']);
 
-const commits = parser.log(getCommits.execSync())
-  // Cleanup commit parents
-  .map(commit => ({
-    ...commit,
-    parents: commit.parents.length ? commit.parents.split(' ') : ['C0'],
-  }))
-  // Convert from Array to Object
-  .reduce((acc, curr) => ({
-    ...acc,
-    [curr.id]: curr,
-  }), {});
+const getBranchesCmd = repository =>
+  new Command(repository, 'branch', ['--format="%(refname:short) %(objectname:)"']);
 
-const branches = getBranches.execSync()
-  .split('\n')
-  .reduce((acc, branch) => {
-    const [id, target] = branch[0] !== '('
-      ? branch.split(' ') // As: "branche/name abcdef01234sha1"
-      : ['HEAD', '']; // TODO: Find real HEAD sha1
+class JSONTree {
+  constructor (path) {
+    this.repository = gitty(path);
+  }
 
-    return id ? {
-      ...acc,
-      [id]: {
-        ...branchTpl,
-        id,
-        target,
-      },
-    } : acc;
-  }, {});
+  getCommits () {
+    return parser.log(getCommitsCmd(this.repository).execSync())
+      // Cleanup commit parents
+      .map(commit => ({
+        ...commit,
+        parents: commit.parents.length ? commit.parents.split(' ') : ['C0'],
+      }))
+      // Convert from Array to Object
+      .reduce((acc, curr) => ({
+        ...acc,
+        [curr.id]: curr,
+      }), {});
+  }
 
-schema.commits = {
-  ...schema.commits,
-  ...commits,
-};
+  getBranches () {
+    return getBranchesCmd(this.repository).execSync()
+      .split('\n')
+      .reduce((acc, branch) => {
+        const [id, target] = branch[0] !== '('
+          ? branch.split(' ') // As: "branche/name abcdef01234sha1"
+          : ['HEAD', '']; // TODO: Find real HEAD sha1
 
-schema.branches = {
-  ...branches,
-};
+        return id ? {
+          ...acc,
+          [id]: {
+            ...branchTpl,
+            id,
+            target,
+          },
+        } : acc;
+      }, {});
+  }
+
+  json () {
+    const schema = { ...schemaTpl };
+    schema.commits = {
+      ...schema.commits,
+      ...this.getCommits(),
+    };
+
+    schema.branches = {
+      ...this.getBranches(),
+    };
+
+    return schema;
+  }
+}
+
+const tree = new JSONTree(process.cwd());
 
 if (process.env.DEBUG === 'true') {
-  console.log(schema); // eslint-disable-line no-console
+  console.log(tree.json()); // eslint-disable-line no-console
 } else {
-  process.stdout.write(JSON.stringify(schema));
+  process.stdout.write(JSON.stringify(tree.json()));
   process.stdout.write('\n');
 }
 
