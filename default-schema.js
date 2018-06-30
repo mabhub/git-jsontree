dda0259030afd8505f35ff165bc00ed30284af26
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-06-30 02:06:27 +0200

    Init first version of source code

---
+++ default-schema.js
@@ -0,0 +1,28 @@
+module.export = {
+  branches: {
+    master: {
+      target: 'C0',
+      id: 'master',
+      remoteTrackingBranchID: null,
+      remote: false,
+      type: 'branch',
+    },
+  },
+  commits: {
+    C0: {
+      parents: [],
+      id: 'C0',
+      rootCommit: true,
+      type: 'commit',
+      author: 'nobody',
+      createTime: 'Fri Jan 01 2000 01:00:00 GMT+0200 (CEST)',
+      commitMessage: 'booh!',
+    },
+  },
+  tags: {},
+  HEAD: {
+    target: 'master',
+    id: 'HEAD',
+    type: 'general ref',
+  },
+};
--- git-learn.js
+++ git-learn.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env node
 
-console.log('Hello world');
+const repository = require('gitty')(process.cwd());
+const Command    = require('gitty/lib/command.js');
+const parser     = require('gitty/lib/parser.js');
+const schema     = require('./default-schema.js');
+
+const logFmt = JSON.stringify({
+  id: '%H',
+  type: 'commit',
+  author: '%an <%ae>',
+  createTime: '%ad',
+  commitMessage: '%s',
+  parents: '%P',
+});
+
+const getCommits = new Command(repository, 'log', [`--pretty=format:'${logFmt}',`, '']);
+
+const commits = parser.log(getCommits.execSync())
+  // Cleanup commit parents
+  .map(commit => ({
+    ...commit,
+    parents: commit.parents.length ? commit.parents.split(' ') : ['C0'],
+  }))
+  // Convert from Array to Object
+  .reduce((acc, curr) => ({
+    ...acc,
+    [curr.id]: curr,
+  }), {});
+
+
+schema.commits = {
+  ...schema.commits,
+  ...commits,
+};
+
+process.stdout.write(JSON.stringify(schema));
+process.stdout.write('\n');
+setTimeout(process.exit, 10);
