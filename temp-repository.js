7d76710b79440a09531e913af829d4e8666942c0
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-07-01 14:23:18 +0200

    Provide some TempRepository metrics through instance

--- temp-repository.js
+++ temp-repository.js
@@ -4,32 +4,55 @@ const tmp      = require('tmp');
 
 class TempRepository {
   constructor () {
+    this.settings = {
+      commitCount: 0,
+      branches: [
+        'master',
+        'myBranch',
+      ],
+      user: {
+        name: 'git-jsontree',
+        email: 'git@jsontree.dev',
+      },
+      comment () {
+        return `git-jsontree (${Math.random()})`;
+      },
+    };
+
     this.tmpDir = tmp.dirSync({ unsafeCleanup: true });
     this.repo = new Gitty(this.tmpDir.name);
     this.path = this.tmpDir.name;
 
     this.repo.initSync();
 
-    const commitCmd = () => `commit --allow-empty -m "git-jsontree (${Math.random()})"`;
-
     const commands = [
-      new Command(this.repo, 'config --local user.name  git-jsontree'),
-      new Command(this.repo, 'config --local user.email git@jsontree.dev'),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, 'checkout -b myBranch HEAD~2'),
-      new Command(this.repo, commitCmd()),
-      new Command(this.repo, commitCmd()),
+      new Command(this.repo, `config --local user.name ${this.settings.user.name}`),
+      new Command(this.repo, `config --local user.email ${this.settings.user.email}`),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, `checkout -b ${this.settings.branches[1]} HEAD~2`),
+      new Command(this.repo, this.commitCmd()),
+      new Command(this.repo, this.commitCmd()),
       new Command(this.repo, 'checkout master'),
-      new Command(this.repo, 'merge myBranch'),
-      new Command(this.repo, commitCmd()),
+      new Command(this.repo, this.mergeCmd()),
+      new Command(this.repo, this.commitCmd()),
     ];
 
     commands.map(cmd => cmd.execSync());
+  }
+
+  commitCmd () {
+    this.settings.commitCount += 1;
+    return `commit --allow-empty -m "${this.settings.comment()}"`;
+  }
+
+  mergeCmd () {
+    this.settings.commitCount += 1;
+    return `merge --no-ff ${this.settings.branches[1]}`;
   }
 
   clean () {
