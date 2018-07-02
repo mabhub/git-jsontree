526e76b5c3f9d752d38cd3f7706d5e69532b63e7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-07-02 22:37:44 +0200

    Upgrade ESLint config to latest release

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,5 @@ module.exports = {
     extends: 'makina',
     env: {
       node: true,
-    },
-    rules: {
-      'import/no-extraneous-dependencies': ['warn', { 'devDependencies': ['**/*.test.js', '**/*.spec.js', , '**/*.stories.js', 'temp-repository.js']}],
     }
 };
